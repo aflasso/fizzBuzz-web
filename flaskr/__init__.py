d22c0e75e6dab5f0a2211dd72936d3264d650ede
aflasso <andresfelipe.lasso@gmail.com>
2024-04-16 22:29:18 -0500

    factory app and database config

---
+++ flaskr/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+
+
+def create_app():
+    
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY= 'dev',
+        DATABASE = os.path.join(app.instance_path, 'fizzBuzz.sqlite')
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+    
+    from . import db
+    db.init_app(app)
+    
+    return app
